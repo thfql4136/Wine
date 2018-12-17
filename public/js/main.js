85d9761e47d4799a81fc0f182212cdd382329bbb
thfql4136 <thfql4136@naver.com>
2018-12-17 14:23:38 +0900

    181217

--- public/js/main.js
+++ public/js/main.js
@@ -107,3 +107,50 @@ clearInterval(interval2);
 	}
 	return WheelScroll;
 }());
+
+var prdNum = 0;
+
+$(".menu").click(function(){ //이벤트 선언
+/* 	$(".news_cont").eq(prdNum).stop().animate({"top":"5%", "opacity":0}, 500 , function(){
+		$(this).css({"display":"none"});
+		
+	}); */
+ prdNum = $(this).index(); //클릭 된 애 값을 받아올꺼야, index는 값을 가져올때 사용
+/*  $(".news_cont").eq(prdNum).css({"display":"block"}).stop().animate({"top":0, "opacity":1}, 500 ); */
+ $(this).css({"color":"#222"});
+ $(".menu_tit").children(".line").css({"width":"0%"});
+$(this).css({"color":"#a22c4d"});
+if($(this).index() !=prdNum){
+	$(this).find(".menu_img1 > img").attr('src', '../img/wine2.png');
+}
+else{
+	$(this).find(".menu_img1 > img").attr('src', '../img/wine.png');
+}
+
+$(".menu_tit").children(".line").css({"width":"100%"});
+});
+
+$(".menu").hover(function(){//이벤트 선언
+	if($(this).index() !=prdNum){ //현재 선택된 애는 제외 시킨다
+		$(this).css({"color":"#a22c4d"});
+		$(".menu_tit").children("div").stop().animate({"width":"100%"}, 100);
+		$(this).find(".menu_img1 > img").attr('src', '../img/wine.png');
+		$(this).find(".menu_img2 > img").attr('src', '../img/wine-bottle.png');
+		$(this).find(".menu_img3 > img").attr('src', '../img/filter.png');
+		$(this).find(".menu_img4 > img").attr('src', '../img/wine-glasses.png');
+		$(this).find(".menu_img5 > img").attr('src', '../img/bucket.png');
+		$(this).find(".menu_img6 > img").attr('src', '../img/wine-barrel.png');
+	}
+},function(){
+	if($(this).index() !=prdNum){
+		$(this).css({"color":"#222"});
+		$(".menu_tit").children("div").stop().animate({"width":"0%"}, 100);
+		$(this).find(".menu_img1 > img").attr('src', '../img/wine2.png');
+		$(this).find(".menu_img2 > img").attr('src', '../img/wine-bottle2.png');
+		$(this).find(".menu_img3 > img").attr('src', '../img/filter2.png');
+		$(this).find(".menu_img4 > img").attr('src', '../img/wine-glasses2.png');
+		$(this).find(".menu_img5 > img").attr('src', '../img/bucket2.png');
+		$(this).find(".menu_img6 > img").attr('src', '../img/wine-barrel2.png');
+	}
+});
+$(".menu").eq(0).trigger("click");
